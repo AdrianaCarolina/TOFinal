454a3b68a45ef162867e151cc95f42bcdb58a817
rodrigo <rmendozamelgarrodrigo@gmail.com>
2015-07-03 00:13:56 -0500

    se completaron los servicios de las entidades

--- TOModel/Movie.cpp
+++ TOModel/Movie.cpp
@@ -1,58 +1,7 @@
-/* 
- * File:   Movie.cpp
- * Author: root
- * 
- * Created on June 20, 2015, 1:25 PM
- */
-
 #include "Movie.h"
 
-Movie::Movie() {
-}
+Movie::Movie()
+{
 
-Movie::Movie(const Movie& orig) {
-}
-
-Movie::~Movie() {
-}
-
-void Movie::setTicketOffice(int ticketOffice) {
-    this->ticketOffice = ticketOffice;
-}
-
-int Movie::getTicketOffice() const {
-    return ticketOffice;
-}
-
-void Movie::setDuration(int duration) {
-    this->duration = duration;
-}
-
-int Movie::getDuration() const {
-    return duration;
-}
-
-void Movie::setClassification(QString classification) {
-    this->classification = classification;
-}
-
-QString Movie::getClassification() const {
-    return classification;
-}
-
-void Movie::setTitle(QString title) {
-    this->title = title;
-}
-
-QString Movie::getTitle() const {
-    return title;
-}
-
-void Movie::setId(int id) {
-    this->id = id;
-}
-
-int Movie::getId() const {
-    return id;
 }
 
--- TOModel/Movie.h
+++ TOModel/Movie.h
@@ -1,39 +1,11 @@
-/* 
- * File:   Movie.h
- * Author: root
- *
- * Created on June 20, 2015, 1:25 PM
- */
-
 #ifndef MOVIE_H
-#define	MOVIE_H
+#define MOVIE_H
 
-#include <QString>
 
-using namespace std;
-
-class Movie {
+class Movie
+{
 public:
     Movie();
-    Movie(const Movie& orig);
-    virtual ~Movie();
-    void setTicketOffice(int ticketOffice);
-    int getTicketOffice() const;
-    void setDuration(int duration);
-    int getDuration() const;
-    void setClassification(QString classification);
-    QString getClassification() const;
-    void setTitle(QString title);
-    QString getTitle() const;
-    void setId(int id);
-    int getId() const;
-private:
-    int id;
-    QString title;
-    QString classification;
-    int duration;
-    int ticketOffice;
 };
 
-#endif	/* MOVIE_H */
-
+#endif // MOVIE_H
